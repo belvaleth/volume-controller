5de610fe92d192478f1545157dc8ac3d88ecf3e6
solidthanatos <solidthanatos@gmail.com>
2020-12-16 23:53:54 +0900

    RC1

--- volume-controller.ino
+++ volume-controller.ino
@@ -5,59 +5,69 @@
 #include <bitset>
 #include <WS2812Serial.h>
 
+// This is needed or compiler will complain about unhandled exceptions
+
 extern "C" {
 	__attribute__((weak)) int __exidx_start() { return -1; }
 	__attribute__((weak)) int __exidx_end() { return -1; }
 }
 
+// Pins for the shift registers
+
 constexpr int latchPin = 21;
 constexpr int clockPin = 19;
 constexpr int dataPin = 18;
 
+// Pins on the Teensy
 constexpr int enc_Master_A = 0;
 constexpr int enc_Master_B = 1;
 constexpr int enc_Master_SW = 2;
-constexpr int enc_Master_Red = 0;
-constexpr int enc_Master_Green = 1;
-constexpr int enc_Master_Blue = 2;
-
 
 constexpr int enc_Current_A = 3;
 constexpr int enc_Current_B = 4;
 constexpr int enc_Current_SW = 5;
-constexpr int enc_Current_Red = 8;
-constexpr int enc_Current_Green = 9;
-constexpr int enc_Current_Blue = 10;
 
-/*
 constexpr int enc_Discord_A = 6;
 constexpr int enc_Discord_B = 7;
 constexpr int enc_Discord_SW = 8;
-constexpr int enc_Discord_Red = 6;
-constexpr int enc_Discord_Green = 7;
-constexpr int enc_Current_Blue = 8;
 
 constexpr int enc_Game_A = 9;
 constexpr int enc_Game_B = 10;
 constexpr int enc_Game_SW = 11;
-constexpr int enc_Game_Red = 9;
-constexpr int enc_Game_Green = 10;
-constexpr int enc_Game_Blue = 11;
 
 constexpr int enc_Music_A = 12;
 constexpr int enc_Music_B = 13;
 constexpr int enc_Music_SW = 14;
-constexpr int enc_Music_Red = 12;
-constexpr int enc_Music_Green = 13;
-constexpr int enc_Music_Blue = 14;
 
 constexpr int enc_Firefox_A = 15;
 constexpr int enc_Firefox_B = 16;
 constexpr int enc_Firefox_SW = 17;
-constexpr int enc_Firefox_Red = 15;
-constexpr int enc_Firefox_Green = 16;
-constexpr int enc_Firefox_Blue = 17;
-*/
+
+// Pins on the daisy chained shift registers. Pins 0-5 on each are used, 6-7 are NC
+
+constexpr int enc_Master_Red = 0;
+constexpr int enc_Master_Green = 1;
+constexpr int enc_Master_Blue = 2;
+
+constexpr int enc_Current_Red = 3;
+constexpr int enc_Current_Green = 4;
+constexpr int enc_Current_Blue = 5;
+
+constexpr int enc_Discord_Red = 8;
+constexpr int enc_Discord_Green = 9;
+constexpr int enc_Discord_Blue = 10;
+
+constexpr int enc_Game_Red = 11;
+constexpr int enc_Game_Green = 12;
+constexpr int enc_Game_Blue = 13;
+
+constexpr int enc_Music_Red = 16;
+constexpr int enc_Music_Green = 17;
+constexpr int enc_Music_Blue = 18;
+
+constexpr int enc_Firefox_Red = 19;
+constexpr int enc_Firefox_Green = 20;
+constexpr int enc_Firefox_Blue = 21;
 
 
 bool masterMute = false;
@@ -75,18 +85,16 @@ std::bitset<16> LEDStream;
 
 // Create encoders
 
-Encoder encMaster(enc_Master_A,enc_Master_B);
-Encoder encCurrent(enc_Current_A,enc_Current_B);
-/*
+Encoder encMaster(enc_Master_A, enc_Master_B);
+Encoder encCurrent(enc_Current_A, enc_Current_B);
 Encoder encDiscord(enc_Discord_A,enc_Discord_B);
 Encoder encGame(enc_Game_A,enc_Game_B);
 Encoder encMusic(enc_Music_A,enc_Music_B);
 Encoder encFirefox(enc_Firefox_A,enc_Firefox_B);
-*/
 
-std::vector<Encoder*> encoders = { &encMaster, &encCurrent };
+std::vector<Encoder*> encoders = { &encMaster, &encCurrent, &encDiscord, &encGame, &encMusic, &encFirefox };
 
-
+// Class to handle changing LED status
 
 class LED
 {
@@ -153,12 +161,16 @@ public:
 
 LED masterLED(enc_Master_Red, enc_Master_Green, enc_Master_Blue, LED::WHITE);
 LED currentLED(enc_Current_Red, enc_Current_Green, enc_Current_Blue, LED::CYAN);
-//LED discordLED(ENC_DISCORD_RED, ENC_DISCORD_GREEN, ENC_DISCORD_BLUE, LED::PURPLE);
-//LED gameLED(ENC_GAME_RED, ENC_GAME_GREEN, ENC_GAME_BLUE, LED::BLUE);
-//LED musicLED(ENC_MUSIC_RED, ENC_MUSIC_GREEN, ENC_MUSIC_BLUE, LED::GREEN);
-//LED firefoxLED(ENC_FIREFOX_RED, ENC_FIREFOX_GREEN, ENC_FIREFOX_BLUE, LED::YELLOW);
+LED discordLED(enc_Discord_Red, enc_Discord_Green, enc_Discord_Blue, LED::PURPLE);
+LED gameLED(enc_Game_Red, enc_Game_Green, enc_Game_Blue, LED::BLUE);
+LED musicLED(enc_Music_Red, enc_Music_Green, enc_Music_Blue, LED::GREEN);
+LED firefoxLED(enc_Firefox_Red, enc_Firefox_Green, enc_Firefox_Blue, LED::YELLOW);
 
-const int numled = 32;
+// numled is # of LED rings * 16 LEDs per ring
+
+const int numled = 96;
+
+// uses pin 20 on the Teensy - this is one of the pins that support DMA
 const int pin = 20;
 
 byte drawingMemory[numled * 3];
@@ -166,7 +178,11 @@ DMAMEM byte displayMemory[numled * 12];
 
 WS2812Serial leds(numled, displayMemory, drawingMemory, pin, WS2812_GRB);
 
+// Colours that the ring will use as it fills. Gradient from green -> yellow -> red
+
 constexpr int RING_COLOURS[16] = { 0x001600, 0x021600, 0x041600, 0x061500, 0x081500, 0x101400, 0x101200, 0x101000, 0x100800, 0x100600, 0x100400, 0x110400, 0x120300, 0x130200, 0x140100, 0x160000 };
+
+// Software debouncing class
 
 class Timer
 {
@@ -219,17 +235,17 @@ void setup()
 
 	pinMode(enc_Master_SW, INPUT);
 	pinMode(enc_Current_SW, INPUT);
-	//pinMode(ENC_DISCORD_SW, INPUT);
-	//pinMode(ENC_GAME_SW, INPUT);
-	//pinMode(ENC_MUSIC_SW, INPUT);
-	//pinMode(ENC_FIREFOX_SW, INPUT);
+	pinMode(enc_Discord_SW, INPUT);
+	pinMode(enc_Game_SW, INPUT);
+	pinMode(enc_Music_SW, INPUT);
+	pinMode(enc_Firefox_SW, INPUT);
 
 	attachInterrupt(digitalPinToInterrupt(enc_Master_SW), muteMaster, RISING);
 	attachInterrupt(digitalPinToInterrupt(enc_Current_SW), muteCurrent, RISING);
-	//attachInterrupt(digitalPinToInterrupt(ENC_DISCORD_SW), muteDiscord, RISING);
-	//attachInterrupt(digitalPinToInterrupt(ENC_GAME_SW), muteGame, RISING);
-	//attachInterrupt(digitalPinToInterrupt(ENC_MUSIC_SW), muteMusic, RISING);
-	//attachInterrupt(digitalPinToInterrupt(ENC_FIREFOX_SW), muteFirefox, RISING);
+	attachInterrupt(digitalPinToInterrupt(enc_Discord_SW), muteDiscord, RISING);
+	attachInterrupt(digitalPinToInterrupt(enc_Game_SW), muteGame, RISING);
+	attachInterrupt(digitalPinToInterrupt(enc_Music_SW), muteMusic, RISING);
+	attachInterrupt(digitalPinToInterrupt(enc_Firefox_SW), muteFirefox, RISING);
 
 	leds.begin();
 
@@ -265,7 +281,7 @@ void loop()
 				reading = (reading * 10);
 			}
 		}
-		updateWS2812(reading, (i*16));
+		updateWS2812(reading, (i * 16));
 		sendString += (String)reading;
 		if (i < (encoders.size() - 1))
 		{
@@ -276,11 +292,13 @@ void loop()
 	delay(10);
 }
 
+// The LED rings are daisy chained off a single data pin, so we need to update them all at once. Offset defined so we only change the bits we need to
+
 void updateWS2812(int volume, int offset)
 {
 	for (int i = 0; i < 16; i++)
 	{
-		leds.setPixel(i+offset, 0x000000);
+		leds.setPixel(i + offset, 0x000000);
 	}
 	int activeLEDs = volume / 60;
 	if (activeLEDs > 16)
@@ -289,11 +307,12 @@ void updateWS2812(int volume, int offset)
 	}
 	for (int i = 0; i < activeLEDs; i++)
 	{
-		leds.setPixel(i+offset, RING_COLOURS[i]);
+		leds.setPixel(i + offset, RING_COLOURS[i]);
 	}
 	leds.show();
 }
 
+// Send updated LED bits through the shift registers
 
 void updateShiftRegister()
 {
@@ -308,7 +327,7 @@ void updateShiftRegister()
 	digitalWrite(latchPin, HIGH);
 }
 
-//button interrupt handlers
+// button interrupt handlers
 
 void muteMaster()
 {
@@ -345,7 +364,7 @@ void muteCurrent()
 		currentTimer.restart();
 	}
 }
-/*
+
 void muteDiscord()
 {
 	if (!discordMute)
@@ -354,11 +373,12 @@ void muteDiscord()
 	}
 	else
 	{
-		encdiscord.write(discordVol);
+		encDiscord.write(discordVol);
 	}
 	discordMute = !discordMute;
 	discordLED.muteToggle();
 }
+
 void muteGame()
 {
 	if (!gameMute)
@@ -372,6 +392,7 @@ void muteGame()
 	gameMute = !gameMute;
 	gameLED.muteToggle();
 }
+
 void muteMusic()
 {
 	if (!musicMute)
@@ -398,4 +419,3 @@ void muteFirefox()
 	firefoxMute = !firefoxMute;
 	firefoxLED.muteToggle();
 }
-*/
